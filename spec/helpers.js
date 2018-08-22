aebdccf1d8953a7155b95db63ba27873f7ca8baf
Ben Russert <benrussert@gmail.com>
2018-08-21 19:40:13 -0600

    Write some realistic tests

---
+++ spec/helpers.js
@@ -0,0 +1,17 @@
+"use babel";
+
+export const executeResultMessage = {
+  header: {
+    msg_id: "808cdffe-17ca-4e16-a075-1392b05d5b51",
+    date: "2018-08-22T01:35:02.856Z",
+    version: "5.2",
+    username: "nteract",
+    msg_type: "execute_reply",
+    session: "82147cdb-471e-4146-aae1-2a276c1d98b4"
+  },
+  metadata: {},
+  parent_header: {},
+  content: { execution_count: 1, data: {} },
+  channel: "shell",
+  buffers: []
+};
---
+++ spec/kernel-middleware-spec.js
@@ -0,0 +1,38 @@
+"use babel";
+
+import { MEDIATYPE, Middleware } from "../lib/kernel-middleware";
+import { executeResultMessage } from "./helpers";
+
+describe("Middleware", () => {
+  let next, code, onResults, middleware;
+  beforeEach(() => {
+    next = jasmine.createSpyObj("next", ["execute"]);
+    code = 'print("Stuck in the middle with you!")';
+    onResults = jasmine.createSpy("onResults");
+
+    middleware = new Middleware();
+    middleware.execute(next, code, onResults);
+    return;
+  });
+
+  it("should call execute once", () => {
+    expect(next.execute.calls.count()).toEqual(1);
+  });
+
+  it("should continue the execution chain", () => {
+    const args = next.execute.calls.argsFor(0);
+
+    expect(args[0]).toBe(code);
+
+    // set up and call the callback
+    const cb = args[1];
+    executeResultMessage.code = code;
+    executeResultMessage.data = {
+      "text/plain": "Stuck in the middle with you!"
+    };
+    const channel = "iopub";
+
+    cb(executeResultMessage, channel);
+    expect(onResults).toHaveBeenCalledWith(executeResultMessage, channel);
+  });
+});
